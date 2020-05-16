1e61d174517ce40197bd5c9d2a5d9214d13c6613
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-05-16 15:16:54 -0300

    Buscando dados da API

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,95 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
-          alt=""
-        />
-        <strong>teste</strong>
-        <span>$190,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao Carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
-          alt=""
-        />
-        <strong>aaaaa</strong>
-        <span>$190,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
-          alt=""
-        />
-        <strong>aaaaa</strong>
-        <span>$190,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
-          alt=""
-        />
-        <strong>aaaaa</strong>
-        <span>$190,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
-          alt=""
-        />
-        <strong>aaaaa</strong>
-        <span>$190,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
-          alt=""
-        />
-        <strong>aaaaa</strong>
-        <span>$190,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{formatPrice(product.priceFormatted)}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>Adicionar ao Carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
