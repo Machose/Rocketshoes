48fbc8c74adc21567f24b80e8bfab4db9021cc38
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-05-16 14:38:08 -0300

    Realizando a estilização do header, cart e home

--- front-end/src/pages/Cart/index.js
+++ front-end/src/pages/Cart/index.js
@@ -1,5 +1,69 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
+                alt=""
+              />
+            </td>
+            <td>
+              <strong>titulo</strong>
+              <span>R$129,00</span>
+            </td>
+
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={15} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,5 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
+          alt=""
+        />
+        <strong>teste</strong>
+        <span>$190,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
+          alt=""
+        />
+        <strong>aaaaa</strong>
+        <span>$190,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
+          alt=""
+        />
+        <strong>aaaaa</strong>
+        <span>$190,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
+          alt=""
+        />
+        <strong>aaaaa</strong>
+        <span>$190,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
+          alt=""
+        />
+        <strong>aaaaa</strong>
+        <span>$190,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgENrtW-iqvyOzaASVkuOoDyxLps4Ov7K3V_Ue6iFhZOtQdSZRAWpi4TmPwGZ3xdOJltHZT9g&usqp=CAc"
+          alt=""
+        />
+        <strong>aaaaa</strong>
+        <span>$190,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
