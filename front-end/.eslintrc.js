a4ba9e49e8d43a9b1e18ebf855368755acfa7301
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-05-17 18:39:02 -0300

    Configurando o reactotron e o reactotron redux para debugar a aplicação

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -22,4 +22,5 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
   },
   'import/prefer-default-export': 'off',
+  'no-console': ['error', { allow: ['tron'] }],
 };
