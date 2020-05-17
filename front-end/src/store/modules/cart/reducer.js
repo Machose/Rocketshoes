377fef645a1c6e23c5f4c0cea67114a7c1bb8e7a
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-05-17 18:58:27 -0300

    Adicionando contagem dinamica de produtos repetidos no carrinho

--- front-end/src/store/modules/cart/reducer.js
+++ front-end/src/store/modules/cart/reducer.js
@@ -1,7 +1,22 @@
+import producer from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.payload];
+      return producer(state, (draft) => {
+        const productIndex = draft.findIndex(
+          (product) => product.id === action.payload.id
+        );
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.payload,
+            amount: 1,
+          });
+        }
+      });
 
     default:
       return state;
