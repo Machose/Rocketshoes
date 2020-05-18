de9fe7677dc71377bed16ae7157d907355d2598f
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-05-18 14:34:48 -0300

    Passando a logica de verificar produtos duplicados do redux para o saga

--- front-end/src/store/modules/cart/reducer.js
+++ front-end/src/store/modules/cart/reducer.js
@@ -4,18 +4,8 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return producer(state, (draft) => {
-        const productIndex = draft.findIndex(
-          (product) => product.id === action.payload.id
-        );
-
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.payload,
-            amount: 1,
-          });
-        }
+        const { payload: product } = action;
+        draft.push(product);
       });
 
     case '@cart/REMOVE':
--- front-end/src/store/modules/cart/sagas.js
+++ front-end/src/store/modules/cart/sagas.js
@@ -1,20 +1,40 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 // call -> chama metodos asincronos que retornam promises
 // put -> dispara uma action do redux
 // all -> cadastra varios listeners (Verifica quando uma action e disparada)
 // takeLatest -> pega sempre a ultima chamada, descartando as anteriores (Counterar usuários que clicam muitas vezes)
+// select -> busca informações dentro do state
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateProductAmountFromCart } from './actions';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
 //funtion* === generator  ->  async funtion
 function* addToCart(action) {
   const { payload: id } = action;
-  const response = yield call(api.get, `/products/${id}`); // yield === await
 
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select((state) =>
+    state.cart.find((p) => p.id === id)
+  );
+
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateProductAmountFromCart(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`); // yield === await
+
+    const { data: product } = response;
+
+    const data = {
+      ...product,
+      amount: 1,
+      priceFormatted: formatPrice(product.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
